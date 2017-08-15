f692f514f8391194481072760dcc7c1d24dc1a94
zziazm <1310726454@qq.com>
2017-08-15 14:58:55 +0800

    Initial Commit

---
+++ TestText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestText
+//
+//  Created by 赵铭 on 2017/8/15.
+//  Copyright © 2017年 zm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestText/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestText
+//
+//  Created by 赵铭 on 2017/8/15.
+//  Copyright © 2017年 zm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
